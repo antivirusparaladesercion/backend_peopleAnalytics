a5fa4096ac10ae9c9550203dac59cda31334b40a
Diego Chavarria <diego.chavarriap@gmail.com>
2020-12-14 17:09:50 -0500

    Seleccion de folder de universidad dinamica, se debe mandar el nombre de la universidad (uniajc.. etc) en el body, como form-data, con una key llamada bucketName

--- server/services/file-upload.js
+++ server/services/file-upload.js
@@ -1,33 +1,36 @@
 // Usa las variables de entorno definidas en el archivo .env
 // Estas variables son: S3_BUCKET, AWS_ACCESS_KEY_ID y AWS_SECRET_ACCESS_KEY
-require('dotenv').config();
-const aws = require('aws-sdk')
-const multer = require('multer')
-const multerS3 = require('multer-s3')
+require("dotenv").config();
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 
 // Se asignan los valores de las variables de entorno a una instancia del objeto aws
 aws.config.update({
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    region: 'us-east-1'
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  region: "us-east-1",
 });
 
 // Creación de un objeto S3
-const s3 = new aws.S3()
- 
+const s3 = new aws.S3();
+
 // En el objeto upload se pasan los parámetros del bucket donde se va a hacer el almacenamiento
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'cargadedatosies/uniajc',
+    bucket: function (req, file, cb) {
+      console.log(req.body.bucketName);
+      cb(null, `cargadedatosies/${req.body.bucketName}`);
+    },
     metadata: function (req, file, cb) {
-      cb(null, {fieldName: 'TESTING_META_DATA'});
+      cb(null, { fieldName: "TESTING_META_DATA" });
     },
     // Se define el nombre del archivo, está de la siguiente manera "marca temporal".csv
     key: function (req, file, cb) {
-      cb(null, Date.now().toString() + '.csv')
-    }
-  })
-})
+      cb(null, Date.now().toString() + ".csv");
+    },
+  }),
+});
 
 module.exports = upload;
